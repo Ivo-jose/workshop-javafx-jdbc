4cbfa47005428b3ee4da1f638ced33afc47bb0c7
Ivo-jose <ivojose19@gmail.com>
2022-01-24 17:57:49 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,10 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	//Criando atributo para ser usado de referência 
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -20,7 +24,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+		    mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX Application");
 			primaryStage.show();
@@ -28,6 +32,11 @@ public class Main extends Application {
 		catch(IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	//Criando método para pegar o atributo private mainScene
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 	
 	public static void main(String[] args) {
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 
@@ -23,15 +32,38 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("onMenuItemDepartmentAction");
+		//Pedindo para o método loadView ler a tela de DepartmentList.fxml
+		loadView("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		//Pedindo para o método loadView ler a tela de About.fxml
+		loadView("/gui/About.fxml");
 	}
 	
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
+	}
+	
+	//Criando um função para abrir outra tela(Pode ser usado em mais de uma tela)
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newvBox = loader.load();
+			//Mostrando a view about na view principal
+			Scene mainScene = Main.getMainScene();
+			//Pegando uma referência para o ScrollPane (MainView)
+			VBox mainVBox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();
+			//Guardando uma referência para o menu
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			//Adicionando os filhos
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newvBox.getChildren());
+			} 
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
