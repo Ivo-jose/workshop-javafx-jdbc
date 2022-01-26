215433ed1b4685235e48c91d6209b7e4b5fb340b
Ivo-jose <ivojose19@gmail.com>
2022-01-26 18:40:38 -0300

    DepartmentForm (dialog) design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -37,8 +46,9 @@ public class DepartmentListController implements Initializable {
 	
 	//Método do tratamento de eventos do click do botão
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAction");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 	}
 	
 	//Método para injeção de dependência de service sem acoplamento forte
@@ -75,5 +85,28 @@ public class DepartmentListController implements Initializable {
 		//Carregando list dentro do obsList
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
+	}
+	
+	//Método auxiliar para criar a tela do DialogForm
+	private void createDialogForm(String absoluteName,Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			//Instanciando um novo Stage para que um palco seja carregado na frente de outro palco
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department data");
+			dialogStage.setScene(new Scene(pane));
+			//Afirmando que a janela não pode ser redimensionada
+			dialogStage.setResizable(false);
+			//Chamando o stage pai da janela
+			dialogStage.initOwner(parentStage);
+			//Dizendo qual o comportamento da janela
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
